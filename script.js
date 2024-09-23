926842471ab7fb15fd2d89a74f6325a609936989
Чынгыз <berdibaevchyngyz96@gmail.com>
2024-09-24 02:17:27 +0600

    .

--- script.js
+++ script.js
@@ -7,14 +7,12 @@ if (menu && menuBtn) {
     menuBtn.addEventListener('click', () => {
         menu.classList.toggle('active')
         menuBtn.classList.toggle('active')
-        body.classList.toggle('lock')
     })
 
     menu.querySelectorAll('menu_link').forEach(link => {
         link.addEventListener('click',()=>{
             menu.classList.remove('active')
             menuBtn.classList.remove('active')
-            body.classList.remove('lock')
         })
     })
 }
